fd57ebf83b4e4666021ac2ff8d258716b27fb251
Victoria Lucena <lucenavictoria0@gmail.com>
2021-11-17 08:56:01 -0300

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,23 @@
+import React from "react";
+import './Header.css';
+
+export default ({black}) => {
+    
+    return(
+      <header className={black ? 'black' : ''}>
+        <div className="header-logo">
+           <a href="/">
+              <img src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg" alt="logo netlifx"></img>
+           </a>
+        </div>
+        <div className="header-user">
+           <a href="/">
+              <img src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png" alt="user"></img>
+           </a>
+        </div>
+      </header>
+    )
+
+
+
+}
